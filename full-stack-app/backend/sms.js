eea5305a7460391b1379dea1522aed32ae773018
praveen seelaboyina <121080561+Spraveen111@users.noreply.github.com>
2024-07-03 23:03:51 +0530

    njjh

---
+++ full-stack-app/backend/sms.js
@@ -0,0 +1,20 @@
+import twilio from "twilio";
+import dotenv from "dotenv";
+dotenv.config();
+
+export default async function sendSMS(messageBody, toPhoneNumber) {
+  const client = new twilio(accountSid, authToken);
+
+  try {
+    const message = await client.messages.create({
+      body: messageBody,
+      to: toPhoneNumber,
+      from: "+17087264376",
+    });
+    console.log(`Message sent with SID: ${message.sid}`);
+    return message.sid; // Return SID or any other meaningful response
+  } catch (error) {
+    console.error(`Failed to send message: ${error.message}`);
+    throw error;
+  }
+}
